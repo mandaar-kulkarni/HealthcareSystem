0daed5d27b59613d8bc8a38b8a6f7964815ae916
mahesh011294 <maheshshinde227@gmail.com>
2021-01-18 05:47:02 +0530

    emergency component added2

---
+++ Registration/src/app/emergency.ts
@@ -0,0 +1,13 @@
+export class Emergency {
+    name:string;
+    symptoms:string;
+    phoneNumber:string;
+
+
+    constructor(name:string,symptoms:string,phoneNumber:string){
+        this.name=name;
+        this.symptoms=symptoms;
+        this.phoneNumber=phoneNumber;
+    }
+
+}
